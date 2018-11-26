f648284055818a518bfea4798d213d00fc43ff70
teagenkiel <teagenkiel@gmail.com>
2018-11-26 16:26:54 -0600

    Update thermostat/thermostat.ino
    Completed home page view and started touch functions

--- thermostat/thermostat.ino
+++ thermostat/thermostat.ino
@@ -8,8 +8,29 @@
 #include <Wire.h>      // this is needed for FT6206
 #include <Adafruit_FT6206.h>
 
-uint16_t backgroundColor = ILI9341_WHITE;
-String modeString = "null";
+// Color definitions
+#define BLACK    0x0000
+#define BLUE     0x001F
+#define RED      0xF800
+#define GREEN    0x07E0
+#define CYAN     0x07FF
+#define MAGENTA  0xF81F
+#define YELLOW   0xFFE0 
+#define WHITE    0xFFFF
+
+uint16_t home_setLeftArrow[] = {60, 176};
+uint16_t home_setRightArrow[] = {138, 176};
+uint16_t home_modeLeftArrow[] = {49, 240};
+uint16_t home_modeRightArrow[] = {169, 240};
+uint16_t home_holdButton[] = {0,0};
+uint16_t home_settingsButton[] = {206, 286};
+
+uint16_t backgroundColor = WHITE;
+int setTemp = 75;
+int currentTemp = 79;
+String modes[] = {"COOL", "HEAT", "AUTO", "OFF"};
+int currentModeIndex = 0;
+String screenState = "";
 
 // home button bitmap 
 const unsigned char homeButton [] PROGMEM = {
@@ -59,48 +80,15 @@ void setup(void) {
     while (1);
   }
 
-//if cooling
-backgroundColor = ILI9341_BLUE;
-modeString = "Cooling";
+  //if cooling
+backgroundColor = WHITE;
 
-tft.setRotation(3);
-homepage();
-  
+tft.setRotation(2);
+homePage();
+
 }
 
-void homepage(){
-  
-  tft.fillScreen(backgroundColor);
-  tft.drawBitmap(280, 10, homeButton, 30, 30, ILI9341_BLACK); // pixel location of homepage: 280-310 x, 10-40 y
-  tft.drawBitmap(280, 75, settingsButton, 30, 30, ILI9341_BLACK); // pixel location of homepage: 280-310 x, 75-105 y
 
-  tft.setTextSize(7);
-  tft.setTextColor(0x0000);
-
-  tft.setCursor(40,40);
-  tft.print("79");
-  tft.print((char)247);
-
-  tft.setCursor(40,150);
-  tft.setTextSize(3);
-  tft.print(modeString);
-  }
-
-void settingsPage(){
-  
-  tft.fillScreen(backgroundColor);
-  tft.drawBitmap(280, 10, homeButton, 30, 30, ILI9341_BLACK); // pixel location of homepage: 280-310 x, 10-40 y
-  tft.drawBitmap(280, 75, settingsButton, 30, 30, ILI9341_BLACK); // pixel location of homepage: 280-310 x, 75-105 y
-  
-  tft.setTextSize(3);
-  tft.setTextColor(0x0000);
-
-  tft.setCursor(40,40);
-  tft.print("Settings");
- 
-  }
-
-  
 void loop() {
   // Wait for a touch
   if (! ctp.touched()) {
@@ -110,17 +98,164 @@ void loop() {
   // Retrieve a point  
   TS_Point p = ctp.getPoint();
 
-  p.x = map(p.x,0,240,240,0);
+  //flip it around to match the screen.
+  //Rotation #3 setting
+  //int oldX = p.x;
+  //int oldY = p.y;
+  //p.x = map(oldY, 0, 320, 0, 320); 
+  //p.y = map(oldX, 0, 240, 240, 0);
 
-// 280-310 x, 10-40 y
-  if (p.x > 0 && p.x < 50 && p.y > 270 && p.y < 320){
-      homepage();
+
+  
+ 
+
+  if(screenState = "HOME"){
+
+    //set temp arrows
+    if(isInTouchZone(home_setLeftArrow, p.x, p.y)){
+      setTemp = setTemp - 1;
+      homePage();
     }
-    
-//280-310 x, 75-105 y
 
-  if (p.x > 65 && p.x < 115 && p.y > 270 && p.y < 320){
+    if(isInTouchZone(home_setRightArrow, p.x, p.y)){
+      setTemp = setTemp + 1;
+      homePage();
+    }
+
+    if(isInTouchZone(home_modeLeftArrow, p.x, p.y)){
+
+      if (currentModeIndex > 0){
+        currentModeIndex -= 1;
+      }
+      else {
+        currentModeIndex = 3;
+      }
+      homePage();
+    }
+
+    if(isInTouchZone(home_modeLeftArrow, p.x, p.y)){
+
+      if (currentModeIndex < 3){
+        currentModeIndex += 1;
+      }
+      else {
+        currentModeIndex = 0;
+      }
+      homePage();
+    }
+
+    
+    //settings button
+    if(isInTouchZone(home_settingsButton, p.x, p.y)){
+
       settingsPage();
     }
 
+    
+
+    
+    
+  }
+
+
+  if(screenState = "SETTINGS"){
+
+  
+  }
+
 }
+
+
+
+void homePage(){
+
+  screenState = "HOME";
+  
+  tft.fillScreen(backgroundColor);
+  tft.setTextColor(BLACK);
+
+  //display date and time
+  tft.setTextSize(2);
+  tft.setCursor(6,8);
+  tft.print("Wed Oct 31 10:46 AM");
+
+  //display current temp
+  tft.setTextSize(11);
+  tft.setCursor(55, 48);
+  tft.print(currentTemp);
+  tft.setTextSize(5);
+  tft.print((char)247);
+
+  //display set to block
+  tft.setTextSize(2);
+  tft.setCursor(80, 152);
+  tft.print("Set To");
+  tft.setTextSize(3);
+  tft.setCursor(home_setLeftArrow[0], home_setLeftArrow[1]);
+  tft.print("< ");
+  tft.print(setTemp);
+  tft.setTextSize(2);
+  tft.print((char)247);
+  tft.setTextSize(3);
+  tft.setCursor(home_setRightArrow[0], home_setRightArrow[1]);
+  tft.print(" >");
+
+  //display mode block
+  tft.setTextSize(2);
+  tft.setCursor(92, 216);
+  tft.print("Mode");
+  tft.setTextSize(3);
+  tft.setCursor(home_modeLeftArrow[0], home_modeLeftArrow[1]);
+  tft.print("< ");
+  tft.print(modes[currentModeIndex]);
+  tft.print(" >");
+
+  //display hold button
+  tft.drawRect(6, 286, 75, 29, BLACK);
+  tft.setTextSize(3);
+  tft.setCursor(9, 291);
+  tft.setTextColor(BLACK);
+  tft.print("HOLD");
+
+  //display settings button
+  tft.drawBitmap(206, 286, settingsButton, 30, 30, BLACK);
+  
+  }
+
+
+
+  
+
+void settingsPage(){
+
+  screenState = "SETTINGS";
+  
+  tft.fillScreen(backgroundColor);
+  tft.drawBitmap(280, 10, homeButton, 30, 30, BLACK); // pixel location of homepage: 280-310 x, 10-40 y
+  
+  tft.setTextSize(3);
+  tft.setTextColor(0x0000);
+
+  tft.setCursor(40,40);
+  tft.print("Settings");
+ 
+  }
+
+bool isInTouchZone(uint16_t coord_arr[], int x_point, int y_point){
+
+    int zone_bound = 30; //makes a square of this many pixels to be touch zone
+
+    if((coord_arr[0] < x_point) && (x_point < (coord_arr[0] + zone_bound))){
+
+      if((coord_arr[1] < y_point) && (y_point < (coord_arr[0] + zone_bound))){
+
+        return true;
+      }
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  
